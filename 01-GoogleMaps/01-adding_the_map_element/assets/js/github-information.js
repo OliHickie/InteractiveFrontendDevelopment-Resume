2e05057e30b470bff833af0a4fa91fec21c9f6f7
OliHickie <oli.h@hotmail.co.uk>
2021-02-07 11:38:15 +0000

    fetch repo info for users

--- 01-GoogleMaps/01-adding_the_map_element/assets/js/github-information.js
+++ 01-GoogleMaps/01-adding_the_map_element/assets/js/github-information.js
@@ -13,6 +13,27 @@ function userInformationHTML(user) {  // console.log(user) will show all the obj
             </div>`; // The above is showing data and linking to github repo. 
 }
 
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No Repos!</div>`;
+    }
+
+    var listItemsHTML = repos.map(function(repo){
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`
+}
+
 function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
@@ -27,11 +48,14 @@ function fetchGitHubInformation(event) {
     );
 
     $.when( // JQ promise
-        $.getJSON(`https://api.github.com/users/${username}`) // function to retrieve github info
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`) // function to retrieve github info
     ).then(
-        function (response) {
-            var userData = response; // show response in user data div
+        function (firstResponse, secondResponse) { // two JSON = two responses
+            var userData = firstResponse[0]; // show response in user data div
+            var repoData = secondResponse[0]; //two resp gets packed as an array, so need the [0]. 
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         }, function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("gh-user-data").html(`<h2>No info found for user ${username}</h2>`); // if response is 404 the return text
